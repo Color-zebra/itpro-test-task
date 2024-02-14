56b3dd70de79d6586c5f9d27016a06aa2db79bd7
Romanenkov Dmitriy <215rd@mail.ru>
2024-02-14 22:47:09 +0300

    feat: columns enum

---
+++ itpro-test-task/src/shared/vars/columns.ts
@@ -0,0 +1,9 @@
+export enum Columns {
+  source = "Источник",
+  author = "Автор",
+  title = "Заголовок",
+  description = "Описание",
+  url = "Ссылка",
+  publishedAt = "Дата публикации",
+  content = "Содержание",
+}
